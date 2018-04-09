c73003c833f03d57e67fde7ecdb6f09ba3243a64
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2018-04-09 02:13:14 -0700

    trying new dating code

--- _schapirozichronos/devscripts/stub.sh
+++ _schapirozichronos/devscripts/stub.sh
@@ -10,10 +10,20 @@ title="${frest#* 5[0-9][0-9][0-9] }"
 hdate="${frest% $title}"
 
 # other tools
+ffprobe="$(ffprobe -v quiet -show_streams -print_format json "$f")"
+date="$(echo "$ffprobe" | jq '.streams[0].tags.creation_time' -r)"
+datefmt='%Y-%m-%dT%H:%M:%SZ'
+if [ "$date" == null ]; then
+    creation="$(gstat --printf="%w" "$f")"
+    echo "$f: $creation" >&2
+    date="$(gdate -u +"$datefmt" -d "$creation")"
+else
+    date="$(date -u +"$datefmt")"
+fi
 mime="$(file --mime-type "$f")"
 mime="${mime##*: }"
-size="$(wc -c "$f" | cut -d' ' -f2)"
-duration_total="$(ffprobe -v quiet -show_streams -print_format json "$f" | jq '.streams[0].duration' -r)"
+size="$(gstat --printf="%s" "$f")"
+duration_total="$(echo "$ffprobe" | jq '.streams[0].duration' -r)"
 duration_frac="${duration_total##*.}"
 duration_total_int="${duration_total%.$duration_frac}"
 duration_m=$(( $duration_total_int / 60 ))
@@ -22,6 +32,7 @@ cat << EOF
 ---
 name: "$fbase"
 order: "$order"
+date: "$date"
 hdate: "$hdate"
 title: "$title"
 keywords: []
